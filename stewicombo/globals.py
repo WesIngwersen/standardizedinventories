40a5fbfa5cda497f948565a920e77ff7662c554d
Mike Liadov <36668577+LiadovERG@users.noreply.github.com>
2018-11-14 15:54:44 -0500

    Update globals.py

--- stewicombo/globals.py
+++ stewicombo/globals.py
@@ -1,15 +1,13 @@
-import re
-#Variables and functions for common use
-INVENTORY_PREFERENCE_BY_COMPARTMENT = {"air":["eGRID","GHGRP","NEI","TRI"],
-                                       "water":["DMR", "TRI"],
-                                       "soil":["TRI"],
-                                       "waste":["RCRAInfo","TRI"]}
-
+DATA_FILEPATH = "data/StewiComboInputNew.xlsx"
 LOOKUP_FIELDS = ["FRS_ID", "Compartment", "SRS_ID"]
+OUTPUT_FILEPATH = "output.csv"
+
 # pandas might infer wrong type, force cast skeptical columns
 FORCE_COLUMN_TYPES = {
     "SRS_CAS": "str"
 }
+
+USE_RELIABILITY_SCORE = True
 
 KEEP_ALL_DUPLICATES =  True
 INCLUDE_ORIGINAL =  True
@@ -19,13 +17,13 @@ COMPARTMENT_COL = "Compartment"
 COL_FUNC_PAIRS = {
     "FacilityID": "join_with_underscore",
     "FlowAmount": "sum",
-    "ReliabilityScore": "reliablity_weighted_sum:FlowAmount"
+    "ReliabilityScore": "reliablity_weighted_sum:FlowAmount",
+    "eGRID_ID": "join_with_underscore"
 }
 COL_FUNC_DEFAULT = "get_first_item"
-
-#Remove substring from inventory name
-def get_id_before_underscore(inventory_id):
-    underscore_match = re.search('_', inventory_id)
-    if underscore_match is not None:
-        inventory_id = inventory_id[0:underscore_match.start()]
-    return inventory_id
+INVENTORY_PREFERENCE_BY_COMPARTMENT = {
+    "air": ["eGRID","GHGRP","NEI","TRI"],
+    "water": ["DMR", "TRI"],
+    "soil": ["TRI"],
+    "waste": ["RCRAInfo","TRI"]
+}
